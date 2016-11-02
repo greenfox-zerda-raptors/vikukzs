e5323568a6ab26ec84d7e1ae7ffa2aee2e6ed5a2
Zsuzsanna Vikuk <vikuk.zs@gmail.com>
2016-11-02 17:53:04 +0100

    workshop 16-21 added

---
+++ week-03/Workshop1/src/Workshop20.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Zsuzska on 2016. 11. 02..
+ */
+public class Workshop20 {
+    public static void main(String... args){
+        // Create a string variable with the following value:
+        String mondat = "We're here at GreenFox's office and I'm a member of the \"Raptors\" team.";
+        // (with the quotation marks ;) )
+
+        System.out.println(mondat);
+    }
+}
---
+++ week-03/Workshop1/src/Workshop21.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Zsuzska on 2016. 11. 02..
+ */
+public class Workshop21 {
+    public static void main(String... args){
+        String proverb = "If it ain't broke, don't fix it.";
+        // Print the "don't fix it" part using the proverb variable
+
+        System.out.println(proverb.substring(19,31));
+    }
+}

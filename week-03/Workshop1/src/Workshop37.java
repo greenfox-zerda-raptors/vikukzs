85bd6f19430cfe77a6ec433c47e9528c6bd9cbfd
Zsuzsanna Vikuk <vikuk.zs@gmail.com>
2016-11-03 20:44:13 +0100

    how the f*%! do i remove the first hashtag

--- week-03/Workshop1/src/Workshop37.java
+++ week-03/Workshop1/src/Workshop37.java
@@ -14,28 +14,18 @@ public class Workshop37 {
         // The # character should be replacable, hence store it in a variable
 
         String hash = "# ";
-        String space= " ";
         for (int n = 1; n <= 7; n++) {
             for (int m = 1; m <= 7; m++) {
-                    System.out.print(hash);
+                System.out.print(hash);
             }
-            System.out.println();
+            if (n % 2 == 0) {
+                System.out.println(hash);
+            }
+            else {
+                System.out.println();
+            }
+
         }
-//        String out = "#";
-//        int i;
-//        int y;
-//        for (y = 0; y < 7; y++) {
-//            if (y % 2 != 0) {
-//                System.out.print(" ");
-//            }
-//            for (i = 0; i <= 7; i++) {
-//                if (i != 7) {
-//                    System.out.print(out + " ");
-//                } else {
-//                    System.out.println("");
-//                }
-//            }
-//        }
     }
 
 }

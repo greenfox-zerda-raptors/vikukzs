b6e3996cfc92b87eaf81f47e553afb30142e8bec
Zsuzsanna Vikuk <vikuk.zs@gmail.com>
2016-11-15 11:21:11 +0100

    WS01 done

---
+++ Week-05/OOPS/src/Bird.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Zsuzska on 2016. 11. 15..
+ */
+public class Bird extends Animal {
+    public void fly(){
+        System.out.println("The bird can fly.");
+    }
+
+}
---
+++ Week-05/OOPS/src/Workshop01.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Zsuzska on 2016. 11. 15..
+ */
+public class Workshop01 extends Animal {
+    public static void main(String... args) {
+        Bird bird = new Bird();
+        bird.fly();
+        bird.eat();
+        bird.sleep();
+    }
+}

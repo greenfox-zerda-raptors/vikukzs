eb43e4e71c4631fa6384db4cfcaef3c312937698
vikukzs <vikuk.zs@gmail.com>
2016-12-09 11:21:15 +0100

    safety commit

--- Week-07/RPG-game/src/Boss.java
+++ Week-07/RPG-game/src/Boss.java
@@ -5,7 +5,7 @@ public class Boss extends HeroAndMonsters {
 
     public Boss(int posX, int posY) {
         super("boss.png", posX, posY);
-        this.name = "King Dalek";
+        this.name = "Captain Hook";
         this.setSP((1 * randSP.nextInt(6)) + 1 + 1);
         this.setHP(2 * 1 * randHP.nextInt(6) + 1 + randHP.nextInt(6) + 1);
         this.setDP(1 / 2 * randDP.nextInt(6)+1 + (randDP.nextInt(6)+1) / 2);
--- Week-07/RPG-game/src/Skeleton.java
+++ Week-07/RPG-game/src/Skeleton.java
@@ -5,7 +5,7 @@ public class Skeleton extends HeroAndMonsters {
 
     public Skeleton(int posX, int posY) {
         super("skeleton.png", posX, posY);
-        this.name = "Dalek";
+        this.name = "Ood";
         this.setSP((1 * randSP.nextInt(6)) + 1);
         this.setHP(2 * 1 * randHP.nextInt(6) + 1);
         this.setDP(1 / 2 * randDP.nextInt(6)+1);

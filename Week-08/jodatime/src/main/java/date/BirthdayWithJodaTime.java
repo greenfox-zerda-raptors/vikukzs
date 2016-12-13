9138d82f0b9ca09b00b13ad88ee62c595f1a24c8
vikukzs <vikuk.zs@gmail.com>
2016-12-13 17:55:39 +0100

    ready with java joda time bd calculator

--- Week-08/jodatime/src/main/java/date/BirthdayWithJodaTime.java
+++ Week-08/jodatime/src/main/java/date/BirthdayWithJodaTime.java
@@ -1,5 +1,7 @@
 package date;
 
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
 import org.joda.time.LocalDate;
 
 import java.util.Scanner;
@@ -12,31 +14,54 @@ public class BirthdayWithJodaTime implements BirthdayCalculator<LocalDate> {
     @Override
     public LocalDate parseDate(String str) {
         // TODO - return with the parsed date; format is: yyyy-MM-dd
-        return null;
+        if (str == null) {
+            throw new NullPointerException();
+        }
+        LocalDate date = new LocalDate(str);
+        return date;
     }
 
     @Override
     public String printMonthAndDay(LocalDate date) {
         // TODO - return the date formatted: month & day (MM. dd.)
-        return null;
+        return date.toString("MM. dd.");
     }
 
     @Override
     public boolean isAnniversaryToday(LocalDate date) {
         // TODO - return with true if today is the same month+day as date
+        LocalDate today = new LocalDate();
+        if (date == null) {
+            throw new NullPointerException();
+        }
+        if (printMonthAndDay(today).equals(date)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public int calculateAgeInYears(LocalDate birthday) {
         // TODO - return how many years age the input date 'birthday' was
-        return -1;
+        LocalDate today = new LocalDate();
+        int yearNow = today.getYear();
+        int yearBirth = birthday.getYear();
+        int age = yearNow - yearBirth;
+        return age;
     }
 
     @Override
     public int calculateDaysToNextAnniversary(LocalDate date) {
         // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
-        return -1;
+        DateTime today = new DateTime();
+        DateTime birthday = new DateTime(today.getYear(), date.getMonthOfYear(), date.getDayOfMonth(), today.getHourOfDay(), today.getMinuteOfHour(), today.getSecondOfMinute()+1);
+
+        Interval daysToNext;
+        if (!birthday.isAfter(today)) {
+            birthday = birthday.plusYears(1);
+        }
+        daysToNext = new Interval(today, birthday);
+        return (int) daysToNext.toDuration().getStandardDays();
     }
 
     public static void main(String[] args) {

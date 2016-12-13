289dc9b3dc4fa5a2f570fa2c659049f311bc4d10
vikukzs <vikuk.zs@gmail.com>
2016-12-13 15:32:47 +0100

    ready with java util time bd calculator

--- Week-08/jodatime/src/main/java/date/BirthdayWithJavaUtilDate.java
+++ Week-08/jodatime/src/main/java/date/BirthdayWithJavaUtilDate.java
@@ -1,38 +1,66 @@
 package date;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public final class BirthdayWithJavaUtilDate implements BirthdayCalculator<Date> {
 
     @Override
     public Date parseDate(String str) {
         // TODO - return with the parsed date; format is: yyyy-MM-dd
-        return null;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date birthDay = format.parse(str);
+            return birthDay;
+        } catch (Exception e) {
+            System.out.println("Unparsable, using " + format);
+            return null;
+        }
     }
 
     @Override
     public String printMonthAndDay(Date date) {
         // TODO - return the date formatted: month & day (MM. dd.)
-        return null;
+        SimpleDateFormat monthDay = new SimpleDateFormat("MM. dd.");
+
+        return monthDay.format(date);
     }
 
     @Override
     public boolean isAnniversaryToday(Date date) {
         // TODO - return with true if today is the same month+day as date
+        Date today = Calendar.getInstance().getTime();
+
+        if (printMonthAndDay(date).equals(today)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public int calculateAgeInYears(Date birthday) {
         // TODO - return how many years age the input date 'birthday' was
-        return -1;
+        Date today = Calendar.getInstance().getTime();
+        int yearNow = today.getYear();
+        int yearBirth = birthday.getYear();
+        int age = yearNow - yearBirth;
+        return age;
     }
 
     @Override
     public int calculateDaysToNextAnniversary(Date date) {
         // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
-        return -1;
+        Date today = Calendar.getInstance().getTime();
+        date.setYear(today.getYear());
+        if (!date.after(today)) {
+            date.setYear(date.getYear()+1);
+        }
+        long difference = date.getTime()-today.getTime();
+
+        return (int) TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
     }
 
     public static void main(String[] args) {

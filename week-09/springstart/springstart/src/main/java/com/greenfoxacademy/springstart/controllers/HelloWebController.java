12ed9ff6b3fddf4d364b2a44e340c1de6024b363
vikukzs <vikuk.zs@gmail.com>
2016-12-19 15:45:41 +0100

    trying to figure out the colors thingy

--- week-09/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-09/springstart/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -12,11 +15,22 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 public class HelloWebController {
     AtomicLong atomicLong = new AtomicLong(1);
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    String[] colors = {"AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro"};
 
     @RequestMapping("/web/greeting")
     public String greeting(Model model) {
+        Random random = new Random();
         model.addAttribute("name", " World");
+        model.addAttribute("hellos", hellos);
+        model.addAttribute("colors", colors);
+        model.addAttribute("random", random);
         model.addAttribute("counter", "This page was loaded " + atomicLong.getAndIncrement() + " times.");
         return "greeting";
     }
+
+
 }

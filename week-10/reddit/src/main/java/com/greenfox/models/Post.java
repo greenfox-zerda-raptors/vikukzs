6fb3cb16625ebaeede7c816b4d8f468580f2e2e8
vikukzs <vikuk.zs@gmail.com>
2017-01-06 15:03:39 +0100

    lomboked post java

--- week-10/reddit/src/main/java/com/greenfox/models/Post.java
+++ week-10/reddit/src/main/java/com/greenfox/models/Post.java
@@ -1,5 +1,7 @@
 package com.greenfox.models;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +10,7 @@ import javax.persistence.Id;
 /**
  * Created by Zsuzska on 2017. 01. 04..
  */
+@Data
 @Entity
 public class Post {
 
@@ -35,43 +38,4 @@ public class Post {
         score--;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
 }
